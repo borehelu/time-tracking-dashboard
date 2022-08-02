649d235bc43859e4cdd9d2f1e75c0066b0444e93
borehelu <borehelu@gmail.com>
2022-08-02 16:25:52 +0300

    added filter state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
+import MainComponent from "./components/MainComponent";
+import DATA from './shared/data.json';
+
 
 function App() {
+  const [data, setData] = useState(DATA);
+  const [active, setActive] = useState('daily');
+
+const handleFilter = (filter) =>{
+  setActive(filter);
+}
+  
+  
   return (
     <div className="wrapper">
-
+      <MainComponent data={ data } active={active} handleFilter = {handleFilter}/>
     </div>
   );
 }
